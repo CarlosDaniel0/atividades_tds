b2fb746cbc7f5237cb63b479770d1b40984c48b9
Daniel <carlos.daniel2016@hotmail.com>
2019-09-25 17:07:40 -0300

    alterações

--- Funcoes/idade.py
+++ Funcoes/idade.py
@@ -1,6 +1,8 @@
 def programa():
+    print('')
     nome = input('Digite o seu nome: ')
     idade = int(input('Digite sua idade: '))
+    print('')
 
     if idade >= 18:
         print('Parabéns', nome,'Você é de maior')
