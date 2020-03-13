54ebea97aac79c669713613726d5588997d65245
carlosdaniel0 <carlos.daniel2016@hotmail.com>
2020-03-13 17:26:03 -0300

    Alterações lista Prof. Osiris

--- lista 2/5_lista2.py
+++ lista 2/5_lista2.py
@@ -10,10 +10,9 @@ l3 = []
 for i in range(len(l1)):
     l1[i] = randint(0,10)
     l3.append(l1[i])
-
-for i in range(len(l2)):
     l2[i] = randint(10, 20)
     l3.append(l2[i])
+
     
 print(l1)
 print(l2)
